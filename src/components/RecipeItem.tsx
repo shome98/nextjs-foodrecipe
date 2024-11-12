c4980119603283037a4eb0b4a80aa4ae951e3f5c
Sowparna Shome <soupornoshome@gmail.com>
2024-11-12 23:44:41 +0530

    next/image to img tag as the image not loading

--- src/components/RecipeItem.tsx
+++ src/components/RecipeItem.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { Recipe } from "./RecipeList";
-import Image from "next/image";
 
 interface GetRecipeDetailsProps{
     getRecipeDetails: Recipe;
@@ -13,7 +12,7 @@ const RecipeItem: React.FC<GetRecipeDetailsProps> = ({ getRecipeDetails }) => {
             <div className="p-6 lg:max-w-6xl max-w-2xl mx-auto">
                 <div className="grid items-start grid-cols-1 lg:grid-cols-2 gap-10">
                     <div className="w-full lg:sticky top-0 sm:flex gap-2">
-                        <Image
+                        <img
                             src={getRecipeDetails?.image}
                             alt={getRecipeDetails?.name}
                             className="w-4/5 rounded object-cover"
