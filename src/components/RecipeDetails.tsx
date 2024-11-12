ce6ea96df693057cddcbe3a17590730ce2cfa20f
Sowparna Shome <soupornoshome@gmail.com>
2024-11-12 23:33:21 +0530

    recipe details component

---
+++ src/components/RecipeDetails.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { Recipe } from "./RecipeList";
+import Image from "next/image";
+
+interface GetRecipeDetailsProps{
+    getRecipeDetails: Recipe;
+}
+
+const RecipeDetailsItem: React.FC<GetRecipeDetailsProps> = ({ getRecipeDetails }) => {
+    return (
+        <div>
+            <Link href={"/recipe-list"}>Go to recipe list</Link>
+            <div className="p-6 lg:max-w-6xl max-w-2xl mx-auto">
+                <div className="grid items-start grid-cols-1 lg:grid-cols-2 gap-10">
+                    <div className="w-full lg:sticky top-0 sm:flex gap-2">
+                        <Image
+                            src={getRecipeDetails?.image}
+                            alt={getRecipeDetails?.name}
+                            className="w-4/5 rounded object-cover"
+                        />
+                    </div>
+                    <div>
+                        <h2 className="text-3xl font-extrabold text-gray-950">
+                            {getRecipeDetails?.name}
+                        </h2>
+                        <div className="gap-4 mt-5">
+                            <p className="text-2xl text-gray-700">
+                                {getRecipeDetails?.mealType[0]}
+                            </p>
+                        </div>
+                        <div className="mt-5">
+                            <p className="text-xl text-gray-800">
+                                {getRecipeDetails?.cuisine}
+                            </p>
+                        </div>
+                        <div className="mt-5">
+                            <h3 className="text-lg font-bold text-gray-700">Ingredients</h3>
+                            <ul className="space-y-3 list-disc mt-4 pl-4 text-sm text-gray-700">
+                                {getRecipeDetails?.ingredients.map((item) => (
+                                    <li key={item}>{item}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default RecipeDetailsItem;
