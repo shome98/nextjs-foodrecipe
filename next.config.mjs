37607fc02952e4edd31f8010f30e307b8184bbc0
Sowparna Shome <soupornoshome@gmail.com>
2024-11-12 23:21:10 +0530

    image domain add

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+  images: {
+    domains: ['cdn.dummyjson.com'], // Add the hostname here
+  },
+};
 
 export default nextConfig;
